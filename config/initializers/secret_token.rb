4a2668f43ec86ff5000b6695fb43125eae82872b
Cornelius Schumacher <schumacher@kde.org>
2012-06-23 10:35:00 +0200

    rails new iwilltravel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwilltravel::Application.config.secret_token = 'e31c8daad96d732aaeaae148cac4421b268ec46b9d1123423aa63525aed6c377bcdd54defdfaaa82dba360e52b7e3ccb11f5d172ca81ffbf1332438746a8b4fc'
