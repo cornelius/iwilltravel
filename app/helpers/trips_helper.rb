ad4108c740c151a7a5fd5f54540dcacb5252e7c1
Cornelius Schumacher <schumacher@kde.org>
2012-06-30 22:59:16 +0200

    Create helpers for date formatting.

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,26 @@
 module TripsHelper
+
+  def trip_date_range trip
+    f = trip.from
+    t = trip.to
+    out = f.strftime( "%B #{f.day.ordinalize}" )
+    if f.year != t.year
+      out += " 2012"
+    end
+    out += " -"
+    if f.year != t.year || f.month != t.month
+      out += t.strftime(" %B")
+    end
+    out += t.strftime( " #{t.day.ordinalize} %Y" )
+  end
+  
+  def trip_title trip
+    if trip.valid?
+      "#{trip.location}, #{trip_date_range(trip)}, " +
+      "#{distance_of_time_in_words(trip.from, trip.to)}"
+    else
+      "Invalid trip data"
+    end
+  end
+
 end
--- test/unit/helpers/trips_helper_test.rb
+++ test/unit/helpers/trips_helper_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class TripsHelperTest < ActionView::TestCase
+  
+  test "format date range" do
+    t = Trip.new
+    t.from = Date.parse "2012-06-29"
+    t.to = Date.parse "2012-07-01"
+    
+    assert_equal "June 29th - July 1st 2012", trip_date_range( t )
+
+    t.to = Date.parse "2012-06-30"
+    assert_equal "June 29th - 30th 2012", trip_date_range( t )
+    
+    t.to = Date.parse "2013-06-30"
+    assert_equal "June 29th 2012 - June 30th 2013", trip_date_range( t )
+    
+  end
+
 end
