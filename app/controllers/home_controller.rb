b414dc71d1d0e934e47808216944b9c3b4097f4b
Cornelius Schumacher <schumacher@kde.org>
2012-07-02 16:12:02 +0200

    Update status when clicking checkboxes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     @trip = Trip.find params[:id]
+
+    @items1 = Item.where( :trip_id  => @trip.id).where( :category => 1 )
+    @items2 = Item.where( :trip_id  => @trip.id).where( :category => 2 )
+    @items3 = Item.where( :trip_id  => @trip.id).where( :category => 3 )
+    @items4 = Item.where( :trip_id  => @trip.id).where( :category => 4 )
   end
   
   def settings
--- app/helpers/details_helper.rb
+++ app/helpers/details_helper.rb
@@ -1,2 +1,11 @@
 module DetailsHelper
+  
+  def details_status items
+    if items
+      "0/#{items.count} done"
+    else
+      "undefined"
+    end
+  end
+
 end
